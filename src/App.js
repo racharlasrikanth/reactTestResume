ea2c46c17af9cb25caa2a6f8033e35e87b3c86be
Srikanth Racharla <srikanth@Srikanths-MacBook-Pro.local>
2021-01-17 10:18:18 +0530

    Some styles added for navbar and some other stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Certifications from "./pages/Certifications/Certifications";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/certifications" component={Certifications} />
       </Switch>
     </div>
   );
---
+++ src/pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+
+const About = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default About;
