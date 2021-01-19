7b7a2a4e0f41d474483e92ba1ff85132cfa25536
Srikanth Racharla <srikanth@Srikanths-MacBook-Pro.local>
2021-01-19 08:26:41 +0530

    Contacts deleted and Some styles added for footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
 import HeroComp from "../../components/HeroComp/HeroComp";
 import NavBar from "../../components/NavBar/NavBar";
@@ -9,7 +8,6 @@ const Home = () => {
     <>
       <NavBar />
       <HeroComp />
-      <Contact />
       <Footer />
     </>
   );
